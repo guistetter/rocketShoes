f887f1fd7cadc2a60502454a6e02efecaff4b6d0
guistetter <guilherme_stetter@hotmail.com>
2020-05-18 17:15:57 -0300

    buscando produtos da api fake e formatando moeda

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,79 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { MdShoppingCart } from "react-icons/md";
 import { ProductList } from "./styles";
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="items"
-        />
-        <strog>Tenis Legal</strog>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="items"
-        />
-        <strog>Tenis Legal</strog>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="items"
-        />
-        <strog>Tenis Legal</strog>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="items"
-        />
-        <strog>Tenis Legal</strog>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="items"
-        />
-        <strog>Tenis Legal</strog>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+import api from "../../services/api";
+import { formatPrice } from "../../utils/format";
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+  async componentDidMount() {
+    const response = await api.get("products");
+    const data = response.data.map((product) => ({
+      //metodo utilizado para formatar preco, poderia ter aplicado apenas: "{formatPrice(product.price)}", sim mas fazer isso dentro do return teria o problema de exectuar este metodo varias vezes.. fazendo no render formatamos o preco apenas uma unica vez, qdo ele é montado! melhor performance do <codigo className=""></codigo>
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strog>{product.title}</strog>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={18} color="#FFF" /> 3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
