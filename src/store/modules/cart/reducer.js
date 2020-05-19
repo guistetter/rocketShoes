1fb63670e9b57a67c3d5d1062e3d2a99ca70e16c
guistetter <guilherme_stetter@hotmail.com>
2020-05-19 11:06:17 -0300

    instalando immer, reconfigurando reducer e tratando produto duplicado no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,15 +1,26 @@
+import produce from "immer";
 export default function cart(state = [], action) {
   console.log("teste", action, state);
   switch (action.type) {
     case "ADD_TO_CART":
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
+
+    default:
+      return state;
+  }
+}
+/*case "ADD_TO_CART":
       return [
         ...state,
         {
           ...action.product,
           amount: 1,
         },
-      ];
-    default:
-      return state;
-  }
-}
+      ];*/
