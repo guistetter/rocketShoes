3250cc7a0073a8184604f03ccc26dba09b679910
guistetter <guilherme_stetter@hotmail.com>
2020-05-19 14:17:20 -0300

    exibindo no card na pagina home o total de produto add no carrinho de compras

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,6 +26,7 @@ class Home extends Component {
   };
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
     return (
       <ProductList>
         {products.map((product) => (
@@ -38,7 +39,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdShoppingCart size={18} color="#FFF" /> 3
+                <MdShoppingCart size={18} color="#FFF" />{" "}
+                {amount[product.id] || 0}
               </div>
               <span>Adicionar ao carrinho</span>
             </button>
@@ -48,6 +50,12 @@ class Home extends Component {
     );
   }
 }
+const mapStateToProps = (state) => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(CartActions, dispatch);
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
